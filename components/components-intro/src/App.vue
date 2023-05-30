5cb467d08db23dc218d29cc3c9c0de9380cc10d1
Leonardo Pimentel Lopes Filho <leo_zinho_filho@hotmail.com>
2023-05-29 21:09:43 -0300

    aula 29-05

---
+++ components/components-intro/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="app">
+        <h1>teste</h1>
+        <p>{{ this.count }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data: function () {
+    return {
+      count:0
+    }
+}
+}
+</script>
+
+<style>
+
+</style>
