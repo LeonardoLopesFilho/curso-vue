91ba4c528a05f949a8ac47ab144cb1ad4e38a2fa
Leonardo Pimentel Lopes Filho <leo_zinho_filho@hotmail.com>
2023-06-13 22:01:07 -0300

    aula dia 13

--- Comunicacao/src/components/Usuario.vue
+++ Comunicacao/src/components/Usuario.vue
@@ -2,10 +2,14 @@
     <div class="container">
         <h1>Componente Usuário</h1>
         <p>Esse é um componente muito legal!</p>
+        <p>Nome é {{ nome }}</p>
         <button @click="alterarNome()">Alterar Nome</button>
         <hr>
         <div class="componentes">
-            <app-usuario-info :nome="nome"/>
+            <app-usuario-info 
+            :nome="nome" 
+            @nomeMudou="nome = $event"
+            :reiniciarFn="reiniciarNome"/>
             <app-usuario-editar />
         </div>
     </div>
@@ -25,6 +29,9 @@ export default {
     methods:{
         alterarNome(){
             this.nome='jão'
+        },
+        reiniciarNome(){
+            this.nome='Pedro'
         }
     }
 }
--- Comunicacao/src/components/UsuarioInfo.vue
+++ Comunicacao/src/components/UsuarioInfo.vue
@@ -3,15 +3,31 @@
         <h2>As Informações de Usuário</h2>
         <p>Vários detalhes...</p>
         <p>Nome do Usuario <strong> {{ inverterNome() }}</strong></p>
+        <button @click="reiniciarNome()">Reiniciar Nome</button>
+        <button @click="reiniciarFN()">Reiniciar CallBack</button>
     </div>
 </template>
 
 <script>
 export default {
-    props:['nome'],
+    props:{
+        nome:{
+            type:String,
+            // required:true
+            // default:function(){
+                //     return Array(10).fill(0).join(',')
+                // }
+            default:'Anonimo'
+        },
+        reiniciarFN: Function
+    },
     methods:{
         inverterNome(){
             return this.nome.split('').reverse().join('')
+        },
+        reiniciarNome(){
+            this.nome = 'Pedro'
+            this.$emit('nomeMudou',this.nome)
         }
     }
 }
